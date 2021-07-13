138c48d466ffc8c70ed1c1c0c639e04fc583f3ac
roushan <roushanverma38@gmail.com>
2021-07-13 12:18:40 +0530

    updated

--- src/Q27.java
+++ src/Q27.java
@@ -1,6 +1,143 @@
 import java.util.*;
+
+interface Computation {
+
+    void Area();
+
+    void perimeter();
+
+    void volume();
+
+}
+
+class Shape implements Computation {
+
+    @Override
+    public void Area() {
+        System.out.println("Area of " + this.getClass().getName().split("\\.")[1] + " is :- ");
+    }
+
+    @Override
+    public void perimeter() {
+//        LabQuestion.Rectangle
+        System.out.println("Perimeter of " + this.getClass().getName().split("\\.")[1] + " is :- ");
+    }
+
+    @Override
+    public void volume() {
+        System.out.println("Volume of " + this.getClass().getName().split("\\.")[1] + " is :- ");
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void Area() {
+        super.Area();
+        var area = Math.PI * Math.pow(radius, 2);
+        System.out.println(area);
+    }
+
+    @Override
+    public void perimeter() {
+        super.perimeter();
+        var perimeter = 2 * Math.PI * radius;
+        System.out.println(perimeter);
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public void Area() {
+        super.Area();
+        var area = length * width;
+        System.out.println(area);
+    }
+
+    @Override
+    public void perimeter() {
+        super.perimeter();
+        var parameter = 2 * (length + width);
+        System.out.println(parameter);
+    }
+}
+
+class Sphere extends Shape {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void Area() {
+        super.Area();
+        var area = 4 * Math.PI * Math.pow(radius, 2);
+        System.out.println(area);
+    }
+
+    @Override
+    public void volume() {
+        super.volume();
+        var volume = (4 / 3) * (Math.PI * Math.pow(radius, 3));
+        System.out.println(volume);
+    }
+}
+
+class ParallelPiped extends Shape {
+    private double length;
+    private double width;
+    private double height;
+
+    public ParallelPiped(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void volume() {
+        super.volume();
+        var volume = 2 * ((length * width) + (width * height) + (height * length));
+        System.out.println(volume);
+    }
+}
+
 public class Q27 {
-    public static void main(String args[]){
-        System.out.println("Pawan");
+    public static void main(String[] args) {
+        var circle = new Circle(2);
+        circle.Area();
+        circle.perimeter();
+
+        System.out.println();
+
+        var rectangle = new Rectangle(2, 6);
+        rectangle.Area();
+        rectangle.perimeter();
+
+        System.out.println();
+
+        var parallelepiped = new ParallelPiped(3, 4, 7);
+        parallelepiped.volume();
+        parallelepiped.Area();
+
+        System.out.println();
+
+        var sphere = new Sphere(45);
+        sphere.Area();
+        sphere.volume();
     }
 }
--- src/Q28.java
+++ src/Q28.java
@@ -1,3 +1,74 @@
 import java.util.*;
+import java.util.regex.Pattern;
+
+class Employee {
+    private String employeeId;
+    private String employeeName;
+    private String employeeDepartment;
+    private String employeeCity;
+    private double employeeSalary;
+
+    /**
+     * @param employeeId         Only in format EMP with M=manager, S=supervisor, A=analyst, C=clerk with 4 digit number
+     * @param employeeName       name
+     * @param employeeDepartment department
+     * @param employeeCity       City
+     * @param employeeSalary     salary
+     */
+    public Employee(String employeeId, String employeeName, String employeeDepartment, String employeeCity, double employeeSalary) {
+        this.employeeId = checkEmployeeId(employeeId.toUpperCase());
+        this.employeeName = employeeName;
+        this.employeeDepartment = employeeDepartment;
+        this.employeeCity = employeeCity;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public void calculateSalary() {
+        var total_salary = 0.0;
+        var HRA = (this.employeeCity.toLowerCase().equals("metro")) ? 30 : 20;
+        var da = getDA(employeeId);
+        if (da != 0) {
+            total_salary = (employeeSalary - (employeeSalary * ((double) HRA / 100))
+                    + (employeeSalary * ((double) da / 100))
+            );
+            System.out.println(total_salary);
+        }
+    }
+
+    private int getDA(String employeeId) {
+        var patterM = ".{3}M.{4}";
+        var patterS = ".{3}S.{4}";
+        var patterA = ".{3}A.{4}";
+        var patterC = ".{3}C.{4}";
+        if (Pattern.matches(patterM, employeeId)) return 120;
+        if (Pattern.matches(patterS, employeeId)) return 110;
+        if (Pattern.matches(patterA, employeeId)) return 100;
+        if (Pattern.matches(patterC, employeeId)) return 90;
+        return 0;
+    }
+
+    private String checkEmployeeId(String employeeId) {
+        try {
+            if (Pattern.matches("EMP[MSAC]\\d{4}", employeeId))
+                return employeeId;
+            else throw new WrongEmployeeException("Pattern don't not match ..");
+        } catch (WrongEmployeeException exception) {
+            exception.printStackTrace();
+            return "";
+        }
+    }
+}
+
+
 public class Q28 {
+    public static void main(String[] args) {
+        var emp = new Employee("EMPM1234", "Nilay", "X", "metro", 2000);
+        emp.calculateSalary();
+    }
+}
+
+class WrongEmployeeException extends Exception {
+    public WrongEmployeeException(String message) {
+
+    }
 }
